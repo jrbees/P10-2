b443b98c9dda41a498e23098cf99f6f8d6b2266c
Jonah Bees <j-bees@onu.edu>
2024-04-18 06:14:34 -0400

    testing- works

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,8 @@
-//Sending to github
-
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-// here pt 1
 int count_letters(const string& str) {
     int count = 0;
     for (char c : str) {
@@ -16,11 +13,33 @@ int count_letters(const string& str) {
     return count;
 }
 
+int count_words(const string& str) {
+    int count = 0;
+    bool in_word = false;
+    for (char c : str) {
+        if (c == ' ') {
+            if (in_word) {
+                count++;
+                in_word = false;
+            }
+        }
+        else {
+            in_word = true;
+        }
+    }
+    // If the last character was part of a word, increment count
+    if (in_word) {
+        count++;
+    }
+    return count;
+}
+
 int main() {
     string input;
     cout << "Enter a string: ";
     getline(cin, input);
     int letter_count = count_letters(input);
-    cout << "Number of letters: " << letter_count << endl;
+    int word_count = count_words(input);
+   
     return 0;
 }
